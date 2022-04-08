cccd200260813cd926eb42fa14fb46a37d11c3bb
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-04-07 21:19:36 -0300

    bugfix
    Correção de sintaxe.

--- FlappyDragon/FlappyDragon/GameViewController.swift
+++ FlappyDragon/FlappyDragon/GameViewController.swift
@@ -17,7 +17,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        stage = view as! SKView
+        stage = (view as! SKView)
         stage.ignoresSiblingOrder = true
         
         presentScene()
